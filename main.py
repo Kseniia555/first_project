80a983e1e1630c7e8d44312314e4ea5ac11f5d7e
Kseniia <arie5@yandex.ru>
2025-11-25 14:13:20 +0300

    Add author function and gitignore

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Hello from repository!')
+# Импорт load_dotenv.
+from dotenv import load_dotenv 
+
+# Импорт библиотеки для работы с окружением.
+import os  
+
+def print_author():
+    load_dotenv() # Загрузка переменных из .env
+    author = os.getenv('AUTHOR')   # Теперь переменные доступны через os.environ
+    print(f"Автор проекта: {author}") 
+
+print_author()
+
